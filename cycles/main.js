b869e12406c14d45ff67dce518274682c286e8a9
Serik <serik@192-168-0-114.local>
2023-01-20 12:42:17 +0600

    for cycles

--- cycles/main.js
+++ cycles/main.js
@@ -1,6 +1,21 @@
 /**
- * Циклдар: For and while
- * Практика: Стадионда жүгіру 5 круг
+ * Циклдар: while and for
+ * Инкремент және дикремент
+ * Практика: Жарыс қосымшасы. Стадионды 5 рет айналып шығу
+ *
  */
 
+// let count = 0;
 
+// while (count < 5) {
+//   count = count + 1;
+//   console.log(count + " айналым бітті");
+// }
+
+// console.log("Жарыс бітті");
+
+for (let count = 1; count < 6; count++) {
+    console.log(count + " айналым бітті");
+}
+
+console.log("Жарыс бітті");
