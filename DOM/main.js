691763fd38625b3c161f52ea3703c486c073e1e6
Serik <serik@Seriks-MacBook-Air.local>
2023-02-14 19:34:41 +0600

    dom intro

---
+++ DOM/main.js
@@ -0,0 +1,8 @@
+/**
+ * DOM - Document object model. HTML-ді Javascript тілі арқылы өзгерту.
+ * Стиль беру
+ * Мәтінді өзгерту
+ */
+
+const h1Element = document.querySelector("h1");
+h1Element.style.color = "green";
